09fab089064b06a6efc6ac35a6a97671f1df34e6
Ian Campbell <ian.campbell87@gmail.com>
2020-12-23 23:59:22 -0500

    Remove whitespace

--- script.py
+++ script.py
@@ -9,20 +9,17 @@ import json
 def auth():
     return os.environ.get("BEARER_TOKEN")
 
-
 def create_url():
-    query = "from:elonmusk"
+    query = "from:elonmusk" # Make dynamic
     tweet_fields = "tweet.fields=author_id,created_at"
     url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
         query, tweet_fields
     )
     return url
 
-
 def create_headers(bearer_token):
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
     return headers
-
 
 def connect_to_endpoint(url, headers):
     response = requests.request("GET", url, headers=headers)
@@ -31,14 +28,12 @@ def connect_to_endpoint(url, headers):
         raise Exception(response.status_code, response.text)
     return response.json()
 
-
 def main():
     bearer_token = auth()
     url = create_url()
     headers = create_headers(bearer_token)
     json_response = connect_to_endpoint(url, headers)
     print(json.dumps(json_response, indent=4, sort_keys=True))
-
 
 if __name__ == "__main__":
     main()
